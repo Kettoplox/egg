e39bbef9d2ad46b1e939d87d914ce052f5363769
Kettoprojects <joaquin420345@gmail.com>
2022-05-08 19:53:37 -0300

    Primer Commit

---
+++ src/sueldos/Sueldos.java
@@ -0,0 +1,17 @@
+package sueldos;
+
+import Entidades.Vendedor;
+import Servicio.VendedorServicio;
+
+
+public class Sueldos {
+
+   
+    public static void main(String[] args) {
+     VendedorServicio vs = new VendedorServicio();
+     Vendedor v1 = vs.altaVendedor();
+     vs.SueldoMensual(v1);
+     vs.vacaciones(v1);
+    }
+    
+}
